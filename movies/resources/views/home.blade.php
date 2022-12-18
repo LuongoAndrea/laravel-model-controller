233471b8c2aed56455e7c2a641b4cd95ccaa510b
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-12-18 23:55:08 +0100

    nuovo progetto Laravel 9

---
+++ movies/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layaut.app')
+@section('page-title', 'home')
+@section('content')
+{{-- @foreach ($comics as $comic)
+<li>{{$comic['title']}}</li> --}}
+
+<div class="d-flex container comics-cards">
+    {{-- @foreach ($comics as $comic)
+
+    @endforeach --}}
+</div>
+
+@endsection
