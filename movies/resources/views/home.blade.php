71d847025572d8114143d388cab55f08edefc168
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-12-19 01:49:31 +0100

    card

--- movies/resources/views/home.blade.php
+++ movies/resources/views/home.blade.php
@@ -2,8 +2,24 @@
 @section('page-title', 'home')
 @section('content')
 
-<div class="d-flex container comics-cards">
-    <ul>
+<div class="d-flex container">
+    <div class="row">
+        @forelse ($movies as $key=>$movie)
+        <div class="col-3 p-4">
+            <div>
+                <img src="{{$movie->image}}" alt="">
+            </div>
+            <div>
+                <h3>{{$movie->title}}</h3>
+            </div>
+        </div>
+        @empty
+        <div>
+            <p>nessun risultato</p>
+        </div>
+        @endforelse
+    </div>
+    {{-- <ul>
         @forelse ($movies as $key=>$movie)
 
         <li>{{$movie->title}}--{{$key}}</li>
@@ -11,7 +27,7 @@
         @empty
         <li>no risultati</li>
         @endforelse
-    </ul>
+    </ul> --}}
 </div>
 
 @endsection
